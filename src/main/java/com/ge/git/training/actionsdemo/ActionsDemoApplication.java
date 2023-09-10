d94987c2936e5b0f078f038b27c0a32f7650e903
Bogdan Solga <bs065744@cerner.net>
2023-09-10 15:31:04 +0300

    [dev] Integrated a small Spring Boot project

---
+++ src/main/java/com/ge/git/training/actionsdemo/ActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ge.git.training.actionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ge/git/training/actionsdemo/ActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ge.git.training.actionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
